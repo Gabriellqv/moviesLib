a5148b33b32c2d1634c6bdf83c840b5835285b6d
Gabriellqv <gabriellqv@gmail.com>
2024-11-06 15:38:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MoviesLib
+
+MoviesLib é um projeto de frontend desenvolvido com React que consome a API do TMDb (The Movie Database) para exibir filmes populares, mais bem avaliados e realizar buscas por filmes.
+
+## Tecnologias Utilizadas
+
+-   **React**: Biblioteca JavaScript para construir interfaces de usuário.
+-   **React Router DOM**: Para navegação entre as páginas.
+-   **Vite**: Ferramenta de construção e empacotamento rápida para React.
+-   **React Icons**: Para ícones personalizados (usado no projeto para exibir estrelas de avaliação).
+-   **PropTypes**: Para validação de props no React.
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+const imagesURL = import.meta.env.VITE_IMG;
+
+const MovieCard = ({ movie, showLink = true }) => {
+    return (
+        <div className="movie-card">
+            <img src={imagesURL + movie.poster_path} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>
+                <FaStar /> {movie.vote_average.toFixed(1)}
+            </p>
+            {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        </div>
+    );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        poster_path: PropTypes.string.isRequired,
+        vote_average: PropTypes.number.isRequired,
+    }).isRequired,
+    showLink: PropTypes.bool,
+};
+
+export default MovieCard;
